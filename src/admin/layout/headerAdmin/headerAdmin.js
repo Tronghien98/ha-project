d6931474471739719897e31afd1a239129f1c7ef
TrongHien98 <andrew.hien98@gmail.com>
2023-11-14 00:27:55 +0700

    gan done header

--- src/admin/layout/headerAdmin/headerAdmin.js
+++ src/admin/layout/headerAdmin/headerAdmin.js
@@ -7,20 +7,18 @@ import user from "../img/icon-user-default.png";
 
 const HeaderAdmin = () => {
   return (
-    <div className="container-fluid">
+    <div className={`container-fluid ${styles.containerHeader}`}>
       <div className="row">
-        <div className="col-3">
+        <div className={`col-3 ${styles.containerLogo}`}>
           <img src={logo} alt="logo" />
         </div>
         <div className="col-7"></div>
-        <div className="col-2">
-          <div className="row">
-            <div className="col-9  p-0 align-item-center">
-              <p className="text-end mb-0 h4">Admin</p>
-            </div>
-            <div className="col-3 justify-content-end align-item-center">
-              <img src={user} alt="admin" className={`${styles.userAvatar}`} />
-            </div>
+        <div className={`col-2 ${styles.containerAvatar}`}>
+          <div className={`p-0 align-item-center ${styles.containerUserName}`}>
+            <p className={`text-end mb-0 h4 ${styles.textUserName}`}>Admin</p>
+          </div>
+          <div className="justify-content-end align-item-center">
+            <img src={user} alt="admin" className={`${styles.userAvatar}`} />
           </div>
         </div>
       </div>
