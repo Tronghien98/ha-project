5823b4d5ccd33092bf8da5c800015496b0c62350
Quyquai <quyquai96@gmail.com>
2023-11-13 21:51:21 +0700

    header.js

---
+++ src/admin/layout/content.js
@@ -0,0 +1,3 @@
+import React, { Fragment } from "react";
+import styles from "./inputAdmin.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react"; // nạp thư viện react
 import ReactDOM from "react-dom"; // nạp thư viện react-dom
 import InputAdmin from "./UI/InputAdmin/inputAdmin";
+import HeaderAdmin from "./admin/layout/headerAdmin";
 
 // Tạo component App
 function App() {
   return (
     <div>
-      <h1> sfcdvd</h1>
-      <InputAdmin label="User name:" type="text"></InputAdmin>
+      <HeaderAdmin></HeaderAdmin>
     </div>
   );
 }
